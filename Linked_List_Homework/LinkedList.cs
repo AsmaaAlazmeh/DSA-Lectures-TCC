e0f6755fbf18aeb67e12da8158459789910effae
mohammad alazmeh <mohamadalazmeh4@gmail.com>
2023-12-12 11:14:09 +0200

    AsmaaAlazmeh

--- Linked_List_Homework/LinkedList.cs
+++ Linked_List_Homework/LinkedList.cs
@@ -24,20 +24,62 @@ namespace Linked_List_Homework
             // consider when the list is empty
 
             // create a new node with the given data
+           
+            if (First == null)
+            {
+                Node newnode = new Node(val);
+                newnode.Data = val;              
+                First = newnode;                
+            }
+            Node move = First;
+            while (move.Next!=null)
+            {
+                move = move.Next;
+            }
+            Node new_node = new Node(val);
+            new_node.Data = val;
+            move.Next = new_node;
+            move = new_node;
         }
         public void RemoveKey(int key)
         {
             // TODO: search for the key and remove it from the list
             // consider when the key does not exist and when the list is empty
+            Node move = First;
+            while (move.Next.Data!=key)
+            {
+                move = move.Next;
+            }
+            move = move.Next.Next;
+            
         }
         public void Merge(LinkedList other_list)
         {
             // TODO: merge this list with the other list
+            Node move = this.First;
+            while (move.Next!=null)
+            {
+                move = move.Next;
+            }
+            move.Next = other_list.First;
         }
         public void Reverse()
         {
             // TODO: revers the nodes of this list
             // initialize three pointers: prev, curr, and next
+            if (First==null || First.Next==null)
+            {
+                return;
+            }
+            Node prev = null, curr = First, next = null;
+            while (curr != null)
+            {
+                next = curr.Next;
+                curr.Next = prev;
+                prev = curr;
+                curr = next;
+            }
+            First = prev;
         }
     }
 }
